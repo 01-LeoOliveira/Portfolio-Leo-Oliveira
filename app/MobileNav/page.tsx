a81472e3b4e32ed9f3ae3b079ba01478a9ff8a37
leonardo.oliveira <leoo2771@gmil.com>
2024-11-22 12:07:33 -0300

    ajuste 3

--- app/MobileNav/page.tsx
+++ app/MobileNav/page.tsx
@@ -1,15 +1,18 @@
-import { XMarkIcon } from "@heroicons/react/20/solid"
 import React from "react"
+import { XMarkIcon } from "@heroicons/react/20/solid"
 
 interface MobileNavProps {
     nav: boolean;
     closeNav: () => void;
 }
 
-export default function MobileNav({ nav, closeNav }: MobileNavProps) {
-    const navAnimation = nav ? "translate-x-0" : "translate-x-[-100%]";
+export default function Page() {
+    // Dummy props for demonstration
+    const nav = false;
+    const closeNav = () => {};
+
     return (
-        <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#09101a]`}>
+        <div className={`fixed ${nav ? "translate-x-0" : "translate-x-[-100%]"} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#09101a]`}>
             <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
                 <a href="#Hero" className="nav-link-mobile">HOME</a>
                 <a href="#About" className="nav-link-mobile">SOBRE</a>
